486c144fdf9cd32af36391671d9c9c501ce5e876
Andrewsolo <andrewsolovyov@gmail.com>
2017-04-11 10:19:21 +0300

    delete Debug\

--- MyRobot_000/ArduinoCore/Debug/src/core/WMath.d
+++
@@ -1,7 +0,0 @@
-src/core/WMath.d src/core/WMath.o: ../src/core/WMath.cpp \
- c:\avr\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- c:\avr\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h
-
-c:\avr\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
-
-c:\avr\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
--- MyRobot_000/ArduinoCore/Debug/src/core/abi.d
+++
@@ -1,7 +0,0 @@
-src/core/abi.d src/core/abi.o: ../src/core/abi.cpp \
- c:\avr\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- c:\avr\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h
-
-c:\avr\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
-
-c:\avr\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
